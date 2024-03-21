47b118f31c25f463eab1dc392d00198ef3de39dd
KittyCat <blueprinterred@gmail.com>
2024-03-21 19:21:52 +0100

    update

--- src/main/java/com/github/worldofmeow/autumn/RequestHandler.java
+++ src/main/java/com/github/worldofmeow/autumn/RequestHandler.java
@@ -3,9 +3,18 @@ package com.github.worldofmeow.autumn;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.IOException;
+
 public class RequestHandler implements HttpHandler {
     @Override
-    public final void handle(HttpExchange exchange) {
+    public final void handle(HttpExchange exchange) throws IOException {
+        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        if (exchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+            exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type,Authorization");
+            exchange.sendResponseHeaders(204, -1);
+            return;
+        }
         request(new SimpleHttpExchange(exchange));
     }
     public void request(SimpleHttpExchange exchange) {
